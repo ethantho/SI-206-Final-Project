1e727f89ada7f5033da8c7085b8ae6f6930c3918
lethan-umich <lethan@umich.edu>
2025-04-14 19:22:02 -0400

    added stock to combined

--- combined_vizualize.py
+++ combined_vizualize.py
@@ -10,37 +10,40 @@ def main(conn: sqlite3.Connection):
     values = conn.execute(
     """
         WITH RECURSIVE generate_series(value) AS (
-            SELECT 1970
+            SELECT 2000
             UNION ALL
             SELECT value + 1 FROM generate_series
-            WHERE value + 1 <= 2025
+            WHERE value + 1 <= 2024
         )
         SELECT 
             value,
             (SELECT COUNT(*) FROM Movies WHERE CAST(release_date AS INT) = value) as movie_count,
-            (SELECT COUNT(*) FROM Games WHERE (release_date / 365 / 24 / 60 / 60 + 1970) = value) as game_count 
+            (SELECT COUNT(*) FROM Games WHERE (release_date / 365 / 24 / 60 / 60 + 1970) = value) as game_count, 
+    		(SELECT AVG(close) FROM Stocks WHERE CAST(date AS INT) = value) as stocks
         FROM generate_series
     """
     ).fetchall()
     years = get_index(values, 0)
     movies = get_index(values, 1)
     games = get_index(values, 2)
+    stocks = get_index(values, 3)
 
     with open("combined.csv", "w") as file:
         csv_file = csv.writer(file, lineterminator="\n")
-        csv_file.writerow(["year", "movie_count", "game_count"])
+        csv_file.writerow(["year", "movie_count", "game_count", "avg_stock_close"])
         csv_file.writerows(values)
 
     width=0.45
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
+    stock_scaling_factor = 20
+
     rect1 = ax.bar(list(map(lambda x: x-width/2, years)), movies, width)
-    rect1.set_label("h")
     rect2 = ax.bar(list(map(lambda x: x+width/2, years)), games, width)
-    rect2.set_label("i")
-    
-    ax.legend((rect1[0], rect2[0]), ("Movies", "Games"))
+    line1 = ax.plot(years, list(map(lambda x: x/stock_scaling_factor, stocks)), "r-")
+
+    ax.legend((rect1[0], rect2[0], line1[0]), ("Movies", "Games", "Avg Stock Price"))
     plt.show()
 
 
